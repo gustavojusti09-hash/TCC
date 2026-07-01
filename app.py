3fec311f7b1e8c8fc36bf2db2bc307fbce7760ea
GUSTAVOJUSTI <gustavojusti09@gmail.com>
2026-07-01 08:58:22 -0300

    versao 1.6

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -18,13 +19,38 @@ def cadastro():
 def movimentacao():
     return render_template('movimentacao.html')
 
-@app.route('/cadastroconcluido.html')
+@app.route('/cadastroconcluido.html', methods=['GET', 'POST'])
 def cadastroconcluido():
+    nome = request.form.get('nome_item')
+    qtde = request.form.get('qtde')
+    estoque_minimo = request.form.get('estoque_minimo')
+    descricao = request.form.get('descricao')
+    preco = request.form.get('preco')
+    foto = request.form.get('foto')
+    categoria = request.form.get('categoria')
+
+    banco = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='',
+        database='senai'
+    )
+
+    cursor = banco.cursor()
+    query = "INSERT INTO estoque (nome, qtde, estoque_minimo, descricao, preco, foto, categoria) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    valores = (nome, qtde, estoque_minimo, descricao, preco, foto, categoria)
+    cursor.execute(query, valores)
+    banco.commit()
+
+    cursor.close()
+    banco.close()
+
     return render_template('cadastroconcluido.html')
 
 @app.route('/teste.html')
 def teste():
     return render_template('teste.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
